02e193d31f8aea8a6da20df34c0ddb42e58927a7
militska <militska@intersvyaz.net>
2020-10-27 21:47:26 +0500

    мини уборка

--- backend/main.go
+++ backend/main.go
@@ -5,14 +5,14 @@ import (
 	"flag"
 	"github.com/gorilla/websocket"
 	"log"
-	"net"
 	"net/http"
-	"os"
 )
 
 var (
-	addr     = flag.String("addr", getIp().String()+":8074", "http service address")
-	upgrader = websocket.Upgrader{
+	addr      = flag.String("addr", getIp().String()+":8074", "http service address")
+	clients   = make(map[*websocket.Conn]bool)
+	broadcast = make(chan Message)
+	upgrader  = websocket.Upgrader{
 		CheckOrigin: func(r *http.Request) bool {
 			return true
 		},
@@ -24,91 +24,28 @@ type Message struct {
 	Username string `json:"username"`
 	Message  string `json:"message"`
 }
-type Connections struct {
-	Con net.Conn
+
+type Chat struct {
+	Message string
+	Name    string
+	//Ip      string
 }
 
-var clients = make(map[*websocket.Conn]bool)
-var broadcast = make(chan Message) // broadcast channel
-
-func echo(w http.ResponseWriter, r *http.Request) {
-	c, err := upgrader.Upgrade(w, r, nil)
-
-	if err != nil {
-		log.Print("upgrade:", err)
-		return
-	}
-	defer c.Close()
-
-	clients[c] = true
-	for {
-		mt, message, err := c.ReadMessage()
-		if err != nil {
-			log.Println("read:", err)
-			break
-		}
-		log.Printf("recv: %s", message)
-
-		msg := Message{Message: "from server " + string(message), Username: "militska", Email: "w"}
-		broadcast <- msg
-		ownmessage := []byte("from server " + string(message))
-		err = c.WriteMessage(mt, ownmessage)
-		if err != nil {
-			log.Println("write:", err)
-			break
-		}
-	}
-}
-
-func chat(w http.ResponseWriter, r *http.Request) {
-	c, err := upgrader.Upgrade(w, r, nil)
-
-	if err != nil {
-		log.Print("upgrade:", err)
-		return
-	}
-	defer c.Close()
-
-	clients[c] = true
-	for {
-		mt, message, err := c.ReadMessage()
-		if err != nil {
-			log.Println("read:", err)
-			break
-		}
-		log.Printf("recv: %s", message)
-
-		data := []byte(message)
-
-		var ch Chat
-		err2 := json.Unmarshal(data, &ch)
-
-		if err2 != nil {
-			log.Println("Unmarshal error:", err2)
-			break
-
-		}
-		log.Print("name:  " + ch.Name)
-
-		msg := Message{Message: "from server " + string(message), Username: "militska", Email: "w"}
-		broadcast <- msg
-		ownmessage := []byte("from server " + string(message))
-		err = c.WriteMessage(mt, ownmessage)
-		if err != nil {
-			log.Println("write:", err)
-			break
-		}
-	}
-}
-
-func handleMessages() {
+/**
+* send message to all connected clients
+ */
+func broadcastSender() {
 	for {
 		msg := <-broadcast
 		for client := range clients {
 			err := client.WriteJSON(msg)
 			if err != nil {
 				log.Printf("error: %v", err)
-				client.Close()
+				err = client.Close()
+				if err != nil {
+					log.Println("broadcastSender. error close client:", err)
+
+				}
 				delete(clients, client)
 			}
 		}
@@ -117,32 +54,13 @@ func handleMessages() {
 
 func main() {
 	log.Print("hi! my ipv4 " + getIp().String())
-
-	go handleMessages()
+	go broadcastSender()
 	flag.Parse()
 	log.SetFlags(0)
 	http.HandleFunc("/echo", echo)
 	http.HandleFunc("/chat", chat)
 
 	log.Fatal(http.ListenAndServe(*addr, nil))
-}
-
-func getIp() net.IP {
-	host, _ := os.Hostname()
-	addrs, _ := net.LookupIP(host)
-	for _, addr := range addrs {
-		if ipv4 := addr.To4(); ipv4 != nil {
-			return ipv4
-		}
-	}
-
-	return nil
-}
-
-type Chat struct {
-	Message string
-	Name    string
-	//Ip      string
 }
 
 func (m *Chat) MarshalBinary() ([]byte, error) {
